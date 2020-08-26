25bddfa31e8b5896052d3bdcee33d3e4ee1575af
Your Name <you@example.com>
2020-08-26 17:33:31 +0900

    スタート画面からのボタンによる遷移

--- Assets/Script/TransitionController.cs
+++ Assets/Script/TransitionController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,8 +11,42 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class TransitionController : MonoBehaviour
 {
+	//BackTo～
 	public void onPressBackToStart()
 	{
 		SceneManager.LoadScene("Start");
+	}
+
+	//Goto～
+
+	public void onPressGoToRanking()
+	{
+		SceneManager.LoadScene("Ranking");
+	}
+
+	public void onPressGoToStageSelectOrInputName()
+	{
+		if (PlayerPrefs.HasKey(Define.USER_NAME))
+		{
+			SceneManager.LoadScene("SelectStage");
+			//ユーザーネームの確認
+			//Debug.Log(PlayerPrefs.GetString(Define.USER_NAME, "0"));
+		}
+        else
+        {
+			SceneManager.LoadScene("InputName");
+			//ユーザーネームの確認
+			//Debug.Log(PlayerPrefs.GetString(Define.USER_NAME, "0"));
+		}
+	}
+
+	public void onPressGoToShop()
+	{
+		SceneManager.LoadScene("Shop");
+	}
+
+	public void onPressGoToSettings()
+	{
+		SceneManager.LoadScene("Settings");
 	}
 }
