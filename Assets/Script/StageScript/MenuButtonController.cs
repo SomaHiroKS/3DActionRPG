66fc31bd6bc7dd29dcd444bc0c46a3c703bf061f
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-09-14 16:43:36 +0900

    Mapのボタンとポップアップウィンドウの実装

--- Assets/Script/StageScript/MenuButtonController.cs
+++ Assets/Script/StageScript/MenuButtonController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MenuButtonController : MonoBehaviour
 {
 	public GameObject menuWindow;
-	
+
 	/// <summary>
 	/// メニューボタンを押すとメニューウィンドウを表示させる
 	/// </summary>
---
+++ Assets/Script/StageScript/PopUpCloseButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpCloseButtonController : MonoBehaviour
+{
+	[SerializeField]
+	public GameObject PopUpWindow;
+
+	public void onPressCloseButton()
+	{
+		PopUpWindow.SetActive(!PopUpWindow.activeInHierarchy);
+	}
+}
